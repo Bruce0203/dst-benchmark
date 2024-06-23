690440906b0c1c8d4983a8adb453e154b2515db7
Bruce0203 <BruceFreedy@gmail.com>
2024-06-23 12:17:41 +0900

    update

--- benches/slice_new.rs
+++ benches/slice_new.rs
@@ -23,32 +23,11 @@ fn benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("benches");
     group.throughput(Throughput::Bytes(1000));
 
-    group.bench_function("bench boxed unsized slice", |b| {
-        b.iter(|| {
-            for _ in 0..1000 {
-                let value: Box<[MaybeUninit<u8>]> = Box::new([MaybeUninit::<u8>::uninit(); 4]);
-                black_box(value);
-            }
-        })
-    });
-
-    let mut vec = std::vec::Vec::<NewTypeOfU8>::with_capacity(4);
-    vec.push(NewTypeOfU8(1));
-    vec.push(NewTypeOfU8(2));
-    vec.push(NewTypeOfU8(3));
-    vec.push(NewTypeOfU8(4));
-
-    group.bench_function("bench alloc vec", |b| {
-        b.iter(|| {
-            for _ in 0..1000 {
-                black_box(std::vec::Vec::<u8>::with_capacity(4));
-            }
-        })
-    });
     group.bench_function("bench sized slice", |b| {
         b.iter(|| {
             for _ in 0..1000 {
-                black_box(Vec::<u8, U4>::uninit());
+                let value = Vec::<u8, U4>::uninit();
+                let _: u8 = *black_box(unsafe { value.get_unchecked_ref(2) });
             }
         })
     });
@@ -56,6 +35,29 @@ fn benchmark(c: &mut Criterion) {
         b.iter(|| {
             for _ in 0..1000 {
                 let value: &[MaybeUninit<u8>] = &[MaybeUninit::<u8>::uninit(); 4];
+                black_box(value);
+                let _: MaybeUninit<u8> = *black_box(unsafe { value.get_unchecked(2) });
+            }
+        })
+    });
+    return;
+
+    group.bench_function("bench boxed unsized slice", |b| {
+        b.iter(|| {
+            for _ in 0..1000 {
+                let value: Box<[MaybeUninit<u8>]> = Box::new([MaybeUninit::<u8>::uninit(); 4]);
+                black_box(unsafe { value.get_unchecked(2) });
+                black_box(value);
+            }
+        })
+    });
+
+    group.bench_function("bench alloc vec", |b| {
+        b.iter(|| {
+            for _ in 0..1000 {
+                let mut value = std::vec::Vec::<u8>::with_capacity(4);
+                unsafe { value.set_len(4) };
+                black_box(unsafe { value.get_unchecked(2) });
                 black_box(value);
             }
         })
